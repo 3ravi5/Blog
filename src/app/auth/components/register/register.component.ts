fd04a73de039d97ff745b8890330fe47190c9071
Ravi Ranjan <3ravi5@gmail.com>
2024-10-18 13:26:35 +0530

    Login and Register components intialized with lazyLoading

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      userName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(16),
+          Validators.minLength(6),
+        ],
+      ],
+    });
+  }
+
+  onSubmit() {
+    console.log('Form submitted', this.form);
+  }
+}
